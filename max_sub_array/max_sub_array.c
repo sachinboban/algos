abf5b1be92801287d41dc230a31505d657511768
Sachin Boban <sachinbobank@gmail.com>
2014-08-27 11:27:56 +0530

    Max sub-array: Adding brute force implementation
    [Development/Enhancement/Defect]
    Development
    
    [Description]
    Implementation of maximum sub-array problem using brute force
    method.
    
    max_sub_array() is a wrapper for max_sub_array_brute() and
    max_sub_array_recursive() subroutines. Depending on the
    mode ALGO_TYPE_LINEAR or ALGO_TYPE_RECURSIVE, max_sub_array(),
    calls either max_sub_array_brute() or max_sub_array_recursive()
    respectively.
    
    [Change]
    Added max_sub_array() and max_sub_array_brute() subroutines.
    
    [Test]
    Compiled successfully.

--- max_sub_array/max_sub_array.c
+++ max_sub_array/max_sub_array.c
@@ -1,6 +1,7 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
+#include<limits.h>
 
 #define ALGO_TYPE_LINEAR 0
 #define ALGO_TYPE_RECURSIVE 1
@@ -11,7 +12,7 @@ struct struct_sub_array {
 	int end_idx;
 	int sum;
 };
-typedef struct_sub_array subarray_t;
+typedef struct struct_sub_array subarray_t;
 
 subarray_t max_sub_array_brute(int *array, int size);
 subarray_t max_cross_sub_array(int* array, int start_idx, int end_idx);
@@ -30,7 +31,7 @@ int main()
 
 	array = malloc(sizeof(int) * size);
 	if(!array) {
-		printf("ERROR: malloc()\n");
+		printf("ERROR: Memory allocation for array\n");
 		return 0;
 	}
 	memset(array, 0, sizeof(int) * size);
@@ -38,8 +39,68 @@ int main()
 	for(index = 0; index < size; index++)
 		scanf("%d", &array[index]);
 
-	max_sub_array(array, size);
+	max_sub_array(array, size, mode);
 
 	free(array);
 	return 0;
+}
+
+int max_sub_array(int *array, int size, int mode)
+{
+	subarray_t max_subarray;
+	int index;
+
+	max_subarray.array = array;
+
+	if(!array) {
+		printf("Error: No Array specified\n");
+		return -1;
+	}
+
+	if(mode == ALGO_TYPE_LINEAR)
+		max_subarray = max_sub_array_brute(array, size);
+	/*else
+		max_subarray = max_sub_array_recursive(array, 0, size - 1);*/
+
+	printf("max sub-array: array[%d....%d, sum[%d]", max_subarray.start_idx,
+			max_subarray.end_idx, max_subarray.sum);
+	for(index = max_subarray.start_idx; index <= max_subarray.end_idx; index++)
+		printf("%d", array[index]);
+
+	return 0;
+}
+
+subarray_t max_sub_array_brute(int* array, int size)
+{
+	 subarray_t max_subarray;
+	 int index;
+	 int sum;
+	 int end_idx;
+
+	max_subarray.array = array;
+	max_subarray.start_idx = -1;
+	max_subarray.end_idx = -1;
+	max_subarray.sum = INT_MIN;
+
+	if(!array) {
+		printf("Error: No Array specified\n");
+		return max_subarray;
+	}
+
+	for(index = 0; index < size; index++) {
+		sum = 0;
+		end_idx = index;
+
+		while(end_idx < size) {
+			sum += array[end_idx];
+			if(max_subarray.sum < sum) {
+				max_subarray.sum = sum;
+				max_subarray.start_idx = index;
+				max_subarray.end_idx = end_idx;
+			}
+			end_idx += 1;
+		}
+	}
+
+	return max_subarray;
 }
