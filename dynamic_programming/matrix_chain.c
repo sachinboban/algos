459efac219769fbcdf37e5836947cd4aadd98d3a
Sachin Boban <sachinbobank@gmail.com>
2014-09-01 10:22:55 +0530

    Matrix chain multiplication: Adding implementation
    [Development/Enhancement/Defect]
    Development
    
    [Description]
    Adding the implementation form matrix_chain()
    
    [Change]
    Added defenition of matrix_chain() via bottom-up approach
    
    [Test]
    Successfull compilation.
    Checkd for few inputs.

--- dynamic_programming/matrix_chain.c
+++ dynamic_programming/matrix_chain.c
@@ -11,7 +11,7 @@
  * Given are dimentions of n matrices A(0)...A(n-1) with dimentions of A(i) as
  * d(i)Xd(i+1). The procedure finds the optimal parenthization for the matrix
  * chain such the number of scalar multiplications is the minimum for the given
- * dimention sequence. The procedure follows bottom-up method, so that before 
+ * dimention sequence. The procedure follows bottom-up method, so that before
  * computing a subproblem all the smaller subproblem it depends on are solved
  * first.
  */
@@ -34,8 +34,84 @@ int main()
 
 	for(idx = 0; idx < dimen_seq_len; idx += 1)
 		scanf("%d", &dimen_seq[idx]);
-	
+
 	matrix_chain(dimen_seq, dimen_seq_len);
-	
+
+	free(dimen_seq);
 	return 0;
+}
+
+void matrix_chain(int *dimen_seq, int dimen_seq_len)
+{
+	int loop, row_idx, col_idx, temp, idx;
+	int n = dimen_seq_len - 1;	//Number of matrices
+	int *cost = NULL;			//Stores the optimal cost
+	int *pos = NULL;			//Stores position of parenthization
+
+	cost = (int *)malloc(sizeof(int) * n * n);
+	if(!cost) {
+		printf("[%s:%d]ERROR: malloc()\n", __FUNCTION__, __LINE__);
+		return;
+	}
+
+	pos = (int *)malloc(sizeof(int) * n * n);
+	if(!pos) {
+		printf("[%s:%d]ERROR: malloc()\n", __FUNCTION__, __LINE__);
+		free(cost);
+		return;
+	}
+
+	memset(cost, 0, sizeof(int) * n * n);
+	memset(pos, 0, sizeof(int) * n * n);
+
+	/* Cost array is calculated diagonally, since we are using the bottom-up
+	 * approach */
+	for(loop = 1; loop < n; loop += 1) {
+		for(row_idx = 0; row_idx < n - loop; row_idx += 1) {
+			int k;
+
+			col_idx = row_idx + loop;
+			idx = row_idx * n + col_idx;
+
+			cost[idx] = INT_MAX;
+
+	/*
+	 * cost(row_idx, col_idx) = min(cost(row_idx, k) + cost(k+1, col_idx)
+	 *	 							+ d(row_idx) * d(k+1) * d(col_idx+1))
+	 */
+			for(k = row_idx; k < col_idx; k += 1) {
+				temp = cost[row_idx * n + k]
+						+ cost[(k + 1) *n + col_idx]
+						+ (dimen_seq[row_idx] * dimen_seq[k + 1]
+							* dimen_seq[col_idx + 1]);
+				if(temp < cost[idx]) {
+					cost[idx] = temp;
+					pos[idx] = k + 1;
+				}
+			}
+		}
+	}
+
+	for(row_idx = 0; row_idx < n; row_idx += 1) {
+		printf("\n");
+		for(col_idx = 0; col_idx < n; col_idx += 1) {
+			if(row_idx >= col_idx)
+				printf("%5.s ", "");
+			else
+				printf("%5.d ", cost[row_idx * n + col_idx]);
+		}
+	}
+
+	for(row_idx = 0; row_idx < n; row_idx += 1) {
+		printf("\n");
+		for(col_idx = 0; col_idx < n; col_idx += 1) {
+			if(row_idx >= col_idx)
+				printf("%5.s ", "");
+			else
+				printf("%5.d ", pos[row_idx * n + col_idx]);
+		}
+	}
+
+	free(pos);
+	free(cost);
 }
