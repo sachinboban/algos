f3bfa5aa60b9b8f214e89b1a5c6494310c5375d7
Sachin Boban <sachinbobank@gmail.com>
2014-09-02 22:53:09 +0530

    rod cutting: Add implementation for bottom-up approach
    [Development/Enhancement/Defect]
    Developement
    
    [Description]
    Added implementation for bottom-up approach
    
    [Change]
    Added rod_cut_bottom_up().
    
    [Test]
    Successfull compilation.
    Working checked with inputs.

--- dynamic_programming/rod_cutting.c
+++ dynamic_programming/rod_cutting.c
@@ -11,8 +11,6 @@
 #include<string.h>
 #include<limits.h>
 
-#define MODE_BOTTOM_UP	0
-#define MODE_TOP_DOWN	1
 #define MALLOC(len)	(int *)malloc(sizeof(int) * len)
 
 /*
@@ -35,19 +33,16 @@ int rod_cut_bottom_up(int *price, int len, int *opt_rev, int *opt_cut);
  * @len			length of the rod
  * @mode		bottom-up or top-down
  *
- * Wrapper function for rod_cut_bottom_up() and rod_cut_top_down(). Depending
- * upon the mode specified, it selects the function for the corresspoding
- * approach.
+ * Wrapper function for rod_cut_bottom_up
  */
-int rod_cut(int *price, int len, int mode);
+int rod_cut(int *price, int len);
 
 int main()
 {
-	int idx, mode, len, ret = 0;
+	int idx, len, ret = 0;
 	int *price = NULL;
 
 	scanf("%d", &len);
-	scanf("%d", &mode);
 
 	price = MALLOC(len);
 	if(!price) {
@@ -58,14 +53,14 @@ int main()
 	for(idx = 0; idx < len; idx += 1)
 		scanf("%d", &price[idx]);
 	
-	ret = rod_cut(price, len, mode);
+	ret = rod_cut(price, len);
 
 	return ret;
 }
 
-int rod_cut(int *price, int len, int mode)
+int rod_cut(int *price, int len)
 {
-	int ret = 0;
+	int idx, ret = 0;
 	int *opt_rev = NULL, *opt_cut = NULL;
 
 	opt_rev = MALLOC(len);
@@ -83,23 +78,19 @@ int rod_cut(int *price, int len, int mode)
 	memset(opt_rev, INT_MIN, len);
 	memset(opt_cut, 0, len);
 
-	switch(mode) {
-		case MODE_BOTTOM_UP:
-			ret = rod_cut_bottom_up(price, len, opt_rev, opt_cut);
-			break;
-
-		/*case MODE_TOP_DOWN:
-			ret = rod_cut_top_down(price, len, opt_rev, opt_cut);
-			break;*/
-		
-		default:
-			printf("[%s]ERROR: unknown mode[%d]\n", __FUNCTION__, mode);
-			ret = 1;
-	}
+	ret = rod_cut_bottom_up(price, len, opt_rev, opt_cut);
 
 	if(ret)
 		goto error_rod_cut_fail;
+
+	printf("Max revenue = %d, Cut: ", opt_rev[len-1]);
 	
+	idx = len;
+	while(idx > 0) {
+		printf("%d ", opt_cut[idx - 1]);
+		idx -= opt_cut[idx - 1];
+	}
+
 error_rod_cut_fail:
 	free(opt_cut);
 error_malloc2:
@@ -107,4 +98,25 @@ error_malloc2:
 error_malloc1:
 
 	return ret;
+}
+
+int rod_cut_bottom_up(int *price, int len, int *opt_rev, int *opt_cut)
+{
+	int i, j, remain, temp;
+
+	for (i = 0; i < len; i += 1) {
+		for(j = 0; j <= i; j += 1) {
+			remain = i - 1 - j;
+			if(remain < 0)
+				temp = price[i];
+			else
+				temp = price[j] + opt_rev[remain];
+			if(temp > opt_rev[i]) {
+				opt_rev[i] = temp;
+				opt_cut[i] = j + 1;
+			}
+		}
+	}
+
+	return 0;
 }
